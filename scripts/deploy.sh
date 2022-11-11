a13c1ef1f5892e8e61ef53c97625415a1d031f8f
曾文馨 <cengwenxin@dyn-160-39-255-210.dyn.columbia.edu>
2022-11-10 21:26:59 -0500

    first

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,20 +7,20 @@ echo "Lambda Deployment Preference: $LAMBDA_DEPLOYMENT_PREFERENCE"
 #     BRANCH="prod"
 # fi
 
-FUNCTION_EXISTS=$(aws lambda wait function-exists --function-name ${LAMBDA_FUNCTION_NAME})
-EXIT_STATUS=$?
-if [ $EXIT_STATUS -ne 0 ]; then
-  echo "The function doesn't exist yet. Creating it..."
-  TARGET_LAMBDA_FUNCTION_VERSION=1
-  echo "Target Version: ${TARGET_LAMBDA_FUNCTION_VERSION}"
-else
-  CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name ${LAMBDA_FUNCTION_NAME} --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
-  echo "New Current Version: ${CURRENT_LAMBDA_FUNCTION_VERSION}"
-  ((CURRENT_LAMBDA_FUNCTION_VERSION++))
-  TARGET_LAMBDA_FUNCTION_VERSION=${CURRENT_LAMBDA_FUNCTION_VERSION}
-  echo "tried incrementing"
-  echo "Target Version: ${TARGET_LAMBDA_FUNCTION_VERSION}"
-fi
+# FUNCTION_EXISTS=$(aws lambda wait function-exists --function-name ${LAMBDA_FUNCTION_NAME})
+# EXIT_STATUS=$?
+# if [ $EXIT_STATUS -ne 0 ]; then
+#   echo "The function doesn't exist yet. Creating it..."
+#   TARGET_LAMBDA_FUNCTION_VERSION=1
+#   echo "Target Version: ${TARGET_LAMBDA_FUNCTION_VERSION}"
+# else
+#   CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name ${LAMBDA_FUNCTION_NAME} --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
+#   echo "New Current Version: ${CURRENT_LAMBDA_FUNCTION_VERSION}"
+#   ((CURRENT_LAMBDA_FUNCTION_VERSION++))
+#   TARGET_LAMBDA_FUNCTION_VERSION=${CURRENT_LAMBDA_FUNCTION_VERSION}
+#   echo "tried incrementing"
+#   echo "Target Version: ${TARGET_LAMBDA_FUNCTION_VERSION}"
+# fi
 
 TARGET_LAMBDA_FUNCTION_CODE="Build.zip"
 zip -rj ${TARGET_LAMBDA_FUNCTION_CODE} function/*
