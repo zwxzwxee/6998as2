2c018f9b64cc98c6c3e675c28cf065503ee04839
曾文馨 <cengwenxin@dyn-160-39-255-210.dyn.columbia.edu>
2022-11-10 21:02:01 -0500

    first

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,14 +7,14 @@ echo "Lambda Deployment Preference: $LAMBDA_DEPLOYMENT_PREFERENCE"
 #     BRANCH="prod"
 # fi
 
-FUNCTION_EXISTS=$(aws lambda wait function-exists --function-name ${LAMBDA_FUNCTION_NAME}-master)
+FUNCTION_EXISTS=$(aws lambda wait function-exists --function-name ${LAMBDA_FUNCTION_NAME})
 EXIT_STATUS=$?
 if [ $EXIT_STATUS -ne 0 ]; then
   echo "The function doesn't exist yet. Creating it..."
   TARGET_LAMBDA_FUNCTION_VERSION=1
   echo "Target Version: ${TARGET_LAMBDA_FUNCTION_VERSION}"
 else
-  CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name ${LAMBDA_FUNCTION_NAME}-master --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
+  CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name ${LAMBDA_FUNCTION_NAME} --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
   echo "New Current Version: ${CURRENT_LAMBDA_FUNCTION_VERSION}"
   ((CURRENT_LAMBDA_FUNCTION_VERSION++))
   TARGET_LAMBDA_FUNCTION_VERSION=${CURRENT_LAMBDA_FUNCTION_VERSION}
@@ -33,7 +33,7 @@ Resources:
   LambdaFunction:
     Type: AWS::Serverless::Function
     Properties:
-      FunctionName: ${LAMBDA_FUNCTION_NAME}-master
+      FunctionName: ${LAMBDA_FUNCTION_NAME}
       Handler: lambda_function.lambda_handler
       Runtime: python3.7
       CodeUri: s3://${S3_BUCKET}/master/${LAMBDA_FUNCTION_NAME}_v${TARGET_LAMBDA_FUNCTION_VERSION}.zip
